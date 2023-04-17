975b20ff224de9c992db820c926cf871cf41b6a3
Sharon Chullunquia Rosas <schullunquia@unsa.edu.pe>
2023-04-17 16:23:17 -0500

    feat: add the inherited properties

--- src/main/kotlin/models/U1.kt
+++ src/main/kotlin/models/U1.kt
@@ -1,6 +1,21 @@
 package models
 
-class U1: Rocket() {
+/**
+ * This class represents the U-1 Rocket.
+ */
+class U1(
+        cost: Float,
+        weight: Float,
+        maxWeight: Float,
+        chanceLaunchExplosion: Float,
+        chanceLandingCrash: Float,
+    ) : Rocket(
+        cost,
+        weight,
+        maxWeight,
+        chanceLaunchExplosion,
+        chanceLandingCrash
+    ) {
     override fun launch(): Boolean {
         TODO("Not yet implemented")
     }
