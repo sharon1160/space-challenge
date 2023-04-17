034224a3c7b7e43efe95fc2f72cdb9ae83f60d8a
Sharon Chullunquia Rosas <schullunquia@unsa.edu.pe>
2023-04-17 15:02:59 -0500

    feat: add Rocket class

--- src/main/kotlin/models/Rocket.kt
+++ src/main/kotlin/models/Rocket.kt
@@ -1,4 +1,21 @@
 package models
 
-class Rocket {
+import interfaces.SpaceShip
+
+class Rocket: SpaceShip {
+    override fun launch(): Boolean {
+        return true
+    }
+
+    override fun land(): Boolean {
+        return true
+    }
+
+    override fun canCarry(item: Item): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun carry(item: Item) {
+        TODO("Not yet implemented")
+    }
 }
