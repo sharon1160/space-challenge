004d9f3b6d053b6178103911de766f434568e503
Sharon Chullunquia Rosas <schullunquia@unsa.edu.pe>
2023-04-17 18:14:49 -0500

    feat: add lunch function and land function

--- src/main/kotlin/models/U2.kt
+++ src/main/kotlin/models/U2.kt
@@ -1,5 +1,7 @@
 package models
 
+import java.util.*
+
 /**
  * This class represents the U-2 Rocket.
  */
@@ -23,7 +25,20 @@ class U2(
      * based on a random number using the probability equation.
      */
     override fun launch(): Boolean {
-        TODO("Not yet implemented")
+        // probability equation
+        chanceLaunchExplosion = 4 * (weight/maxWeight)
+
+        // random number between 1 and 100
+        val randomNumber = Random().nextInt(100) + 1
+
+        // True if the launch was successful
+        // otherwise, False
+        return if (chanceLaunchExplosion != null) {
+            chanceLaunchExplosion!! <  randomNumber
+        }
+        else {
+            false
+        }
     }
 
     /**
@@ -32,6 +47,19 @@ class U2(
      * the probability equation.
      */
     override fun land(): Boolean {
-        TODO("Not yet implemented")
+        // probability equation
+        chanceLandingCrash = 8 * (weight/maxWeight)
+
+        // random number between 1 and 100
+        val randomNumber = Random().nextInt(100) + 1
+
+        // True if the landing was successful
+        // otherwise, False
+        return if (chanceLandingCrash != null) {
+            chanceLandingCrash!! <  randomNumber
+        }
+        else {
+            false
+        }
     }
 }
