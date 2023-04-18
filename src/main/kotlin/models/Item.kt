76c356f7ee0b2d6a007ef3f56c80161015779bee
Sharon Chullunquia Rosas <schullunquia@unsa.edu.pe>
2023-04-18 12:28:00 -0500

    feat: add function to subtract weight

--- src/main/kotlin/models/Item.kt
+++ src/main/kotlin/models/Item.kt
@@ -3,4 +3,11 @@ package models
 /**
  * This class represents an item to be carried by the rockets.
  */
-class Item (var name: String, var weight: Float)
+class Item (
+            var name: String,
+            var weight: Float
+    ) {
+    fun decreaseWeight(difference: Float) {
+        weight -= difference
+    }
+}
