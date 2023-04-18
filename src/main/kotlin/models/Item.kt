5fb9e3c893b7292c7051c60a65f8d70934a9e4b6
Sharon Chullunquia Rosas <schullunquia@unsa.edu.pe>
2023-04-18 08:37:52 -0500

    feat: change data type

--- src/main/kotlin/models/Item.kt
+++ src/main/kotlin/models/Item.kt
@@ -3,4 +3,4 @@ package models
 /**
  * This class represents an item to be carried by the rockets.
  */
-class Item (var name: String, var weight: Int)
+class Item (var name: String, var weight: Float)
